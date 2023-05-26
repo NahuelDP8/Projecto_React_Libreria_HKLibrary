927f7213b5cdc68082c47a43fa22caadc9f45386
NahuelDP8 <ndp912@gmail.com>
2023-05-26 10:26:51 -0300

    Navbar y imports para que funcione boostrap
    Co-Authored-By: JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,5 +1,18 @@
+'use client';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 export default function NavbarApp(){
-    return <h1>
-        NAVBAR archivo
-    </h1>
+    return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">HK Library</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <h1>
-      HOME
+      Page description
     </h1>
   )
 }
