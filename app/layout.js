b39a718b511de6e0d668182e607b3786d4338876
JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>
2023-06-29 14:04:56 -0300

    PWA funciona
    nahue god
    
    Co-Authored-By: Dueñas Pacioni Nahuel <89594152+NahuelDP8@users.noreply.github.com>

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,48 @@ import NavbarApp from './components/navbar/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/globalStyles.css';
 
+const APP_NAME = "HKLibrary";
+const APP_DEFAULT_TITLE = "HKL";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Una app que atrapa tu alma!";
+
+export const metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
