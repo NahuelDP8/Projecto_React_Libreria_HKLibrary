98066c631fc55a8d31bc542f04d5a316115f6a54
NahuelDP8 <ndp912@gmail.com>
2023-06-29 00:28:26 -0300

    correciones
    Co-Authored-By: JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>

--- app/services/LibraryClientApi.js
+++ app/services/LibraryClientApi.js
@@ -4,7 +4,7 @@ import AuthCookieManager from "./AuthCookieManager";
 
 export default class LibraryClientApi{
     constructor(){
-        //this.BASE_URL="https://pixel-pioneers-laravel-git-entregapromocion-pixel-pioneer.vercel.app";
+        //this.BASE_URL="https://pixel-pioneers-laravel-git-mercadopago-pixel-pioneer.vercel.app";
         this.BASE_URL="http://localhost:8000"
         this.API_URL_BASE="/rest/v1";
 
