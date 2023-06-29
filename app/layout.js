9b5c881777ff0fbe9cf6d0dc100c4fbdef01c051
JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>
2023-06-29 14:07:51 -0300

    remove metadata
    Co-Authored-By: Dueñas Pacioni Nahuel <89594152+NahuelDP8@users.noreply.github.com>

--- app/layout.js
+++ app/layout.js
@@ -9,42 +9,42 @@ const APP_DEFAULT_TITLE = "HKL";
 const APP_TITLE_TEMPLATE = "%s - PWA App";
 const APP_DESCRIPTION = "Una app que atrapa tu alma!";
 
-export const metadata = {
-  applicationName: APP_NAME,
-  title: {
-    default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
-  },
-  description: APP_DESCRIPTION,
-  manifest: "/manifest.json",
-  themeColor: "#FFFFFF",
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "default",
-    title: APP_DEFAULT_TITLE,
-    // startUpImage: [],
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  openGraph: {
-    type: "website",
-    siteName: APP_NAME,
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-  twitter: {
-    card: "summary",
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-};
+// export const metadata = {
+//   applicationName: APP_NAME,
+//   title: {
+//     default: APP_DEFAULT_TITLE,
+//     template: APP_TITLE_TEMPLATE,
+//   },
+//   description: APP_DESCRIPTION,
+//   manifest: "/manifest.json",
+//   themeColor: "#FFFFFF",
+//   appleWebApp: {
+//     capable: true,
+//     statusBarStyle: "default",
+//     title: APP_DEFAULT_TITLE,
+//     // startUpImage: [],
+//   },
+//   formatDetection: {
+//     telephone: false,
+//   },
+//   openGraph: {
+//     type: "website",
+//     siteName: APP_NAME,
+//     title: {
+//       default: APP_DEFAULT_TITLE,
+//       template: APP_TITLE_TEMPLATE,
+//     },
+//     description: APP_DESCRIPTION,
+//   },
+//   twitter: {
+//     card: "summary",
+//     title: {
+//       default: APP_DEFAULT_TITLE,
+//       template: APP_TITLE_TEMPLATE,
+//     },
+//     description: APP_DESCRIPTION,
+//   },
+// };
 
 const inter = Inter({ subsets: ['latin'] })
 
